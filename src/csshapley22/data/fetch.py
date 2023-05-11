69489cca427b193d09c3066280d7b6606edaebc8
Markus Semmler <markus.semmler@unternehmertum.de>
2023-05-11 02:05:29 +0200

    Add preprocess stage.

--- src/csshapley22/data/fetch.py
+++ src/csshapley22/data/fetch.py
@@ -15,94 +15,28 @@ from csshapley22.utils import set_random_seed
 logger = setup_logger()
 
 
-def fetch_dataset(dataset_name: str, dataset_kwargs: Dict) -> Tuple[Dataset, Dataset]:
+def fetch_dataset(dataset_name: str, dataset_kwargs: Dict):
     dataset_idx = make_hash_sha256(dataset_kwargs)
-    dataset_folder = Config.DATASET_PATH / dataset_idx
+    dataset_folder = Config.RAW_PATH / dataset_idx
     validation_set_path = str(dataset_folder / "validation_set.pkl")
     test_set_path = str(dataset_folder / "test_set.pkl")
 
-    if not dataset_folder.exists():
-        os.makedirs(str(dataset_folder))
-        logger.info(f"Dataset {dataset_name} doesn't exist.")
-        logger.debug(f"Dataset config is \n{dataset_kwargs}.")
-        set_random_seed(dataset_kwargs.get("seed", 42))
+    os.makedirs(str(dataset_folder), exist_ok=True)
+    logger.info(f"Dataset {dataset_name} doesn't exist.")
+    logger.debug(f"Dataset config is \n{dataset_kwargs}.")
+    set_random_seed(dataset_kwargs.get("seed", 42))
 
-        dataset_kwargs.pop("preprocessor", None)
-        validation_set, test_set = create_openml_dataset(**dataset_kwargs)
+    dataset_kwargs.pop("preprocessor", None)
+    validation_set, test_set = create_openml_dataset(**dataset_kwargs)
 
-        with open(str(dataset_folder / "dataset_kwargs.json"), "w") as file:
-            json.dump(dataset_kwargs, file, sort_keys=True, indent=4)
+    with open(str(dataset_folder / "dataset_kwargs.json"), "w") as file:
+        json.dump(dataset_kwargs, file, sort_keys=True, indent=4)
 
-        for set_path, set in [
-            (validation_set_path, validation_set),
-            (test_set_path, test_set),
-        ]:
-            with open(set_path, "wb") as file:
-                pickle.dump(set, file)
+    for set_path, set in [
+        (validation_set_path, validation_set),
+        (test_set_path, test_set),
+    ]:
+        with open(set_path, "wb") as file:
+            pickle.dump(set, file)
 
-        logger.info(f"Stored dataset '{dataset_name}' on disk.")
-
-    else:
-        with open(validation_set_path, "rb") as file:
-            validation_set = pickle.load(file)
-
-        with open(test_set_path, "rb") as file:
-            test_set = pickle.load(file)
-
-        logger.info(f"Loaded dataset '{dataset_name}' from disk.")
-        logger.debug(f"Dataset config is \n{dataset_kwargs}.")
-
-    return validation_set, test_set
-
-
-def preprocess_dataset(
-    dataset_name: str, dataset_kwargs: Dict
-) -> Tuple[Dataset, Dataset]:
-    dataset_idx = make_hash_sha256(dataset_kwargs)
-    dataset_folder = Config.DATASET_PATH / dataset_idx
-    validation_set_path = str(dataset_folder / "validation_set.pkl")
-    test_set_path = str(dataset_folder / "test_set.pkl")
-
-    if not dataset_folder.exists():
-        logger.info(f"Dataset {dataset_name} doesn't exist.")
-        logger.debug(f"Dataset config is \n{dataset_kwargs}.")
-        set_random_seed(dataset_kwargs.get("seed", 42))
-
-        preprocessor_definitions = dataset_kwargs.pop("preprocessor", None)
-        validation_set, test_set = create_openml_dataset(**dataset_kwargs)
-
-        if preprocessor_definitions is not None:
-            for (
-                preprocessor_name,
-                preprocessor_kwargs,
-            ) in preprocessor_definitions.items():
-                preprocessor = PreprocessorRegistry[preprocessor_name]
-                validation_set, test_set = preprocessor(
-                    validation_set, test_set, **preprocessor_kwargs
-                )
-
-        os.makedirs(str(dataset_folder))
-
-        with open(str(dataset_folder / "dataset_kwargs.json"), "w") as file:
-            json.dump(dataset_kwargs, file, sort_keys=True, indent=4)
-
-        for set_path, set in [
-            (validation_set_path, validation_set),
-            (test_set_path, test_set),
-        ]:
-            with open(set_path, "wb") as file:
-                pickle.dump(set, file)
-
-        logger.info(f"Stored dataset '{dataset_name}' on disk.")
-
-    else:
-        with open(validation_set_path, "rb") as file:
-            validation_set = pickle.load(file)
-
-        with open(test_set_path, "rb") as file:
-            test_set = pickle.load(file)
-
-        logger.info(f"Loaded dataset '{dataset_name}' from disk.")
-        logger.debug(f"Dataset config is \n{dataset_kwargs}.")
-
-    return validation_set, test_set
+    logger.info(f"Stored dataset '{dataset_name}' on disk.")
--- src/csshapley22/experiments/preprocess_data.py
+++ src/csshapley22/experiments/preprocess_data.py
@@ -1,167 +1,29 @@
-import logging
-from datetime import datetime
-from pathlib import Path
-
 import click
 from dvc.api import params_show
-from dvc.repo import Repo
 
 from csshapley22.constants import RANDOM_SEED
-from csshapley22.experiments.all import experiment_noise_removal, experiment_wad
+from csshapley22.data.fetch import fetch_dataset
+from csshapley22.data.preprocess import preprocess_dataset
 from csshapley22.log import setup_logger
-from csshapley22.preprocess import (
-    parse_datasets_config,
-    parse_models_config,
-    parse_valuation_methods_config,
-)
-from csshapley22.utils import set_random_seed, timeout
+from csshapley22.utils import set_random_seed
 
 logger = setup_logger()
-
 set_random_seed(RANDOM_SEED)
-
-DOUBLE_BREAK = 120 * "="
-SINGLE_BREAK = 120 * "-"
 
 
 @click.command()
-def run():
-    logger.info("Starting data valuation experiment")
+def preprocess_data():
+    logger.info("Starting downloading of data.")
 
     params = params_show()
-    logger.info(f"Using parameters:\n{params}")
     general_settings = params["general"]
 
-    # preprocess valuation methods to be callable from utility to valuation result.
-    valuation_methods = general_settings["valuation_methods"]
-    valuation_methods_factory = parse_valuation_methods_config(valuation_methods)
-
-    # preprocess datasets
+    # fetch datasets
     datasets_settings = general_settings["datasets"]
-    datasets = parse_datasets_config(datasets_settings)
-
-    # preprocess models_config
-    models_config = general_settings["models"]
-    model_generator_factory = parse_models_config(models_config)
-
-    n_repetitions = general_settings["n_repetitions"]
-    date_iso_format = datetime.now().isoformat()
-
-    # Create the output directory
-    experiment_output_dir = (
-        Path(Repo.find_root()) / "output" / "results" / date_iso_format
-    )
-
-    for repetition in range(n_repetitions):
-        logger.info(DOUBLE_BREAK)
-        repetition_output_dir = experiment_output_dir / f"{repetition=}"
-
-        for model_name in models_config.keys():
-            logger.info(f"Executing repetition {repetition} for model '{model_name}'.")
-            experiments_output_dir = repetition_output_dir / model_name
-
-            _run_and_measure_experiment_one(
-                datasets,
-                model_name,
-                model_generator_factory,
-                valuation_methods_factory,
-                experiments_output_dir,
-            )
-
-            _run_and_measure_experiment_two(
-                datasets,
-                model_name,
-                model_generator_factory,
-                valuation_methods_factory,
-                experiments_output_dir,
-            )
-
-            experiment_three_settings = params["experiment_3"]
-            experiment_three_path = experiments_output_dir / "value_transfer"
-
-            for test_model_name in experiment_three_settings["test_models"]:
-                _run_and_measure_experiment_three(
-                    datasets,
-                    model_name,
-                    test_model_name,
-                    model_generator_factory,
-                    valuation_methods_factory,
-                    experiment_three_path,
-                )
-
-    logger.info("Finished data valuation experiment")
-
-
-def _run_and_measure_experiment_one(
-    datasets,
-    model_name,
-    model_generator_factory,
-    valuation_methods_factory,
-    experiments_output_dir,
-):
-    logger.info(SINGLE_BREAK)
-    logger.info(f"Task 1: Weighted absolute difference.")
-    logger.info(SINGLE_BREAK)
-
-    model = model_generator_factory[model_name]()
-    experiment_one_path = experiments_output_dir / "wad"
-    results = experiment_wad(
-        model=model,
-        datasets=datasets,
-        valuation_methods_factory=valuation_methods_factory,
-    )
-    results.store(experiment_one_path)
-    logger.info(f"Results are {results}.")
-    logger.info("Stored results of experiment one.")
-
-
-def _run_and_measure_experiment_two(
-    datasets,
-    model_name,
-    model_generator_factory,
-    valuation_methods_factory,
-    experiments_output_dir,
-):
-    logger.info(DOUBLE_BREAK)
-    logger.info(f"Calculating task 2: Noise removal for classification.")
-    model = model_generator_factory[model_name]()
-    experiment_two_path = experiments_output_dir / "noise_removal"
-    results = experiment_noise_removal(
-        model=model,
-        datasets=datasets,
-        valuation_methods_factory=valuation_methods_factory,
-    )
-    results.store(experiment_two_path)
-    logger.info(f"Results are {results}.")
-    logger.info("Stored results of experiment two.")
-
-
-def _run_and_measure_experiment_three(
-    datasets,
-    model_name,
-    test_model_name,
-    model_generator_factory,
-    valuation_methods_factory,
-    experiment_three_path,
-):
-    logger.info(DOUBLE_BREAK)
-    logger.info(
-        f"Calculating task 3: Value transfer to dfifferent model implementations."
-    )
-    logger.info(f"Testing against model '{test_model_name}'.")
-    model = model_generator_factory[model_name]()
-    test_model = model_generator_factory[test_model_name]()
-    experiment_three_test_path = experiment_three_path / test_model_name
-    results = experiment_wad(
-        model=model,
-        datasets=datasets,
-        valuation_methods_factory=valuation_methods_factory,
-        test_model=test_model,
-    )
-    results.store(experiment_three_test_path)
-    logger.info(f"Results are {results}.")
-    logger.info(f"Stored results of experiment three against'{test_model_name}'.")
+    for dataset_name, dataset_kwargs in datasets_settings.items():
+        logger.info(f"Fetching dataset {dataset_name} with kwargs {dataset_kwargs}.")
+        preprocess_dataset(dataset_name, dataset_kwargs)
 
 
 if __name__ == "__main__":
-    run()
+    preprocess_data()

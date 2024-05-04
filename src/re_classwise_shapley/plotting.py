33fa0a761a30ee957231199ef87fd8c33334c381
Miguel de Benito Delgado <m.debenito@appliedai.de>
2024-05-04 21:58:58 +0200

    Sort boxplots and labels, homogeneize styles

--- src/re_classwise_shapley/plotting.py
+++ src/re_classwise_shapley/plotting.py
@@ -1,6 +1,5 @@
 import math as m
 from contextlib import contextmanager
-from functools import reduce
 from typing import Any, Callable, List, Literal, Sequence, Tuple, Union
 
 import matplotlib.pyplot as plt
@@ -198,7 +197,7 @@ def plot_grid_over_datasets(
         n_rows, n_cols, figsize=(n_cols * patch_size[0], n_rows * patch_size[1])
     )
     plt.subplots_adjust(
-        left=0.05, right=0.95, top=0.9, bottom=0.05, wspace=0.3, hspace=0.3
+        left=0.05, right=0.95, top=0.9, bottom=0.05, wspace=0.2, hspace=0.3
     )
     ax = ax.flatten()
 
@@ -265,14 +264,14 @@ def plot_grid_over_datasets(
 
             if not isinstance(legend, list):
                 fig.legend(
-                    *list(ax[0].get_legend_handles_labels()),
-                    **d,
-                )
+                        *list(ax[0].get_legend_handles_labels()),
+                        **d,
+                        )
             else:
                 fig.legend(
-                    handles=legend,
-                    **d,
-                )
+                        handles=legend,
+                        **d,
+                        )
 
     plt.tight_layout()
     yield fig
@@ -281,11 +280,11 @@ def plot_grid_over_datasets(
 
 @contextmanager
 def plot_histogram(
-    data: pd.DataFrame,
-    method_names: OneOrMany[str],
-    patch_size: Tuple[float, float] = (3, 2.5),
-    n_cols: int = 5,
-) -> plt.Figure:
+        data: pd.DataFrame,
+        method_names: OneOrMany[str],
+        patch_size: Tuple[float, float] = (3, 2.5),
+        n_cols: int = 5,
+        ) -> plt.Figure:
     """
     Plot the histogram of the data values for each dataset and valuation method.
 
@@ -336,7 +335,7 @@ def plot_histogram(
         plot_histogram_func,
         patch_size=patch_size,
         n_cols=n_cols,
-        legend=True,
+        legend=False,
         method_names=ensure_list(method_names),
         xlabel="Value",
         ylabel="#",
@@ -364,26 +363,39 @@ def plot_time(
     Returns:
         A figure containing the plot.
     """
+    data.loc[:, "method_name"] = data["method_name"].apply(lambda m: LABELS[m])
 
     def plot_time_func(data: pd.DataFrame, ax: plt.Axes, **kwargs):
-        data.loc[:, "method_name"] = data["method_name"].apply(lambda m: LABELS[m])
-        sns.boxplot(
-            data=data,
-            x="time_s",
-            y="method_name",
-            hue="method_name",
-            palette=COLOR_ENCODING,
-            legend=False,
-            width=0.5,
-            ax=ax,
+        for method_name in sorted(data["method_name"].unique()):
+            method_data = data[data["method_name"] == method_name]
+            sns.boxplot(
+                data=method_data,
+                x="time_s",
+                y="method_name",
+                color=COLORS[COLOR_ENCODING[method_name]][0],
+                legend=False,
+                width=0.8,
+                linewidth=0.5,
+                showfliers=False,
+                ax=ax,
+                )
+        ax.set_yticklabels([])
+        ax.tick_params(axis="y", length=0)
+
+    legend_patches = [
+        Patch(
+            color=COLORS[COLOR_ENCODING[method]][0],
+            label=method
         )
+        for method in sorted(data["method_name"].unique())
+    ]
 
     with plot_grid_over_datasets(
         data,
         plot_time_func,
         patch_size=patch_size,
         n_cols=n_cols,
-        legend=False,
+        legend=legend_patches,
         xlabel="s",
         ylabel="",
         tick_params_left_only=True,
@@ -416,7 +428,11 @@ def plot_curves(
     def plot_curves_func(data: pd.DataFrame, ax: plt.Axes, **kwargs):
         dataset_name = kwargs.get("dataset_name")
         data.loc[:, "method_name"] = data["method_name"].apply(lambda m: LABELS[m])
-        for method_name, method_data in data.groupby("method_name"):
+
+        grouped_data = data.groupby("method_name")
+
+        for method_name in sorted(grouped_data.groups.keys()):
+            method_data = grouped_data.get_group(method_name)
             color_name = COLOR_ENCODING[method_name]
             mean_color, shade_color = COLORS[color_name]
 
@@ -485,30 +501,38 @@ def plot_metric_boxplot(
         n_cols: Number of columns for subplot layout.
     """
 
+    data = data[data["method_name"] != "random"]
+    data.loc[:, "method_name"] = data["method_name"].apply(lambda m: LABELS[m])
+
     def plot_metric_boxplot_func(data: pd.DataFrame, ax: plt.Axes, **kwargs):
-        data.loc[:, "method_name"] = data["method_name"].apply(lambda m: LABELS[m])
-        sns.boxplot(
-            data=data,
-            x="metric",
-            y="method_name",
-            hue="method_name",
-            palette={
-                label: COLORS[COLOR_ENCODING[label]][0] for label in LABELS.values()
-            },
-            legend=False,
-            bootstrap=10000,
-            width=0.5,
-            ax=ax,
-        )
+        grouped_data = data.groupby("method_name")
+
+        for method_name in sorted(grouped_data.groups.keys()):
+            method_data = grouped_data.get_group(method_name)
+            sns.boxplot(
+                data=method_data,
+                x="metric",
+                y="method_name",
+                hue="method_name",
+                palette={
+                    label: COLORS[COLOR_ENCODING[label]][0] for label in LABELS.values()
+                },
+                legend=False,
+                bootstrap=10000,
+                width=0.8,
+                linewidth=0.5,
+                showfliers=False,
+                ax=ax,
+            )
         ax.set_yticklabels([])
         ax.tick_params(axis="y", length=0)
 
     legend_patches = [
         Patch(
-            color=COLORS[COLOR_ENCODING[LABELS[method]]][0],
-            label=LABELS[method],
+            color=COLORS[COLOR_ENCODING[method]][0],
+            label=method
         )
-        for method in data["method_name"].unique()
+        for method in sorted(data["method_name"].unique())
     ]
 
     with plot_grid_over_datasets(
